eaaa019974a60bd67f550ef34b6be144b27d8d90
tailow <tomi.salonen01@outlook.com>
2025-11-10 21:09:20 +0200

    Added DS18B20 printing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -16,6 +16,7 @@ use esp_hal::{
     timer::timg::TimerGroup,
 };
 use esp_radio::ble::controller::BleConnector;
+use onewire::{self, DS18B20, DeviceSearch, OneWire};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -49,7 +50,7 @@ fn main() -> ! {
             .expect("Failed to initialize Wi-Fi controller");
     let _connector = BleConnector::new(&radio_init, peripherals.BT, Default::default());
 
-    let delay = Delay::new();
+    let mut delay = Delay::new();
 
     let od_for_dht11 = Output::new(
         peripherals.GPIO4,
@@ -64,20 +65,92 @@ fn main() -> ! {
 
     let mut dht11 = Dht11::new(od_for_dht11, delay);
 
-    loop {
-        delay.delay(Duration::from_millis(2000));
+    let mut onewire_pin = Output::new(
+        peripherals.GPIO16,
+        Level::Low,
+        OutputConfig::default().with_drive_mode(DriveMode::OpenDrain),
+    )
+    .into_flex();
 
-        match dht11.read() {
-            Ok(sensor_reading) => log::info!(
-                "DHT 11 Sensor - Temperature: {} °C, humidity: {} %",
-                sensor_reading.temperature,
-                sensor_reading.humidity
-            ),
-            Err(error) => {
-                log::error!("An error occurred while trying to read sensor: {:?}", error)
+    onewire_pin.set_input_enable(true);
+
+    let mut wire = OneWire::new(onewire_pin, false);
+
+    let mut first_iteration = true;
+
+    'infinite: loop {
+        // Just a little trick to prevent spam, in case we need to restart this loop
+        if !first_iteration {
+            delay.delay(Duration::from_secs(1)); // Prevent spam
+        } else {
+            first_iteration = false;
+        }
+
+        // Reset to test if wire is okay and if any sensor is connected
+        if wire
+            .reset(&mut delay)
+            .inspect_err(|_| log::error!("Failed to reset wire"))
+            .is_err()
+        {
+            continue 'infinite;
+        }
+
+        // Start searching for a sensor (we just care to get the first one)
+        let mut search = DeviceSearch::new();
+        let Ok(device) = wire
+            .search_next(&mut search, &mut delay)
+            .inspect_err(|_| log::error!("Failed to search for temperature sensor"))
+        else {
+            continue;
+        };
+        let Some(device) = device else {
+            log::info!("No temperature sensor found");
+            continue;
+        };
+
+        log::info!("Found temperature sensor: {:?}", device);
+
+        // Construct the sensor driver
+        let Ok(sensor) = DS18B20::new(device)
+            .inspect_err(|_| log::error!("Failed to create temperature sensor"))
+        else {
+            continue;
+        };
+
+        'measure: loop {
+            let Ok(resolution) = sensor
+                .measure_temperature(&mut wire, &mut delay)
+                .inspect_err(|_| log::error!("Failed to start temperature measurement"))
+            else {
+                continue 'measure;
+            };
+
+            delay.delay(Duration::from_millis(resolution.time_ms() as u64));
+
+            // Retrieve the measured temperature from the sensor
+            let Ok(raw_temperature) = sensor
+                .read_temperature(&mut wire, &mut delay)
+                .inspect_err(|_| log::error!("Failed to read temperature"))
+            else {
+                continue 'measure;
+            };
+
+            // Process and log the temperature
+            let (integer, fraction) = onewire::ds18b20::split_temp(raw_temperature);
+            let temperature = (integer as f32) + (fraction as f32) / 10000.0;
+
+            log::info!("DS18B20 - Temperature: {} °C", temperature);
+
+            match dht11.read() {
+                Ok(sensor_reading) => log::info!(
+                    "DHT 11 - Temperature: {} °C, humidity: {} %",
+                    sensor_reading.temperature,
+                    sensor_reading.humidity
+                ),
+                Err(error) => {
+                    log::error!("An error occurred while trying to read sensor: {:?}", error)
+                }
             }
         }
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
